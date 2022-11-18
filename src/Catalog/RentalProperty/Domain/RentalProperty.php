cf18a680e6d43c0c9b9c73bb4cb771a7d13c3152
Adrián García <akrian231@gmail.com>
2022-11-18 12:35:45 +0100

    abstract class property

--- src/Catalog/RentalProperty/Domain/RentalProperty.php
+++ src/Catalog/RentalProperty/Domain/RentalProperty.php
@@ -2,67 +2,9 @@
 
 namespace App\Catalog\RentalProperty\Domain;
 
-use App\Catalog\Shared\Domain\Property\PropertyCharacteristics;
-use App\Catalog\Shared\Domain\Property\PropertyDescription;
-use App\Catalog\Shared\Domain\Property\PropertyGallery;
-use App\Catalog\Shared\Domain\Property\PropertyId;
-use App\Catalog\Shared\Domain\Property\PropertyLocation;
-use App\Catalog\Shared\Domain\Property\PropertyTitle;
-use DateTime;
+use App\Catalog\Shared\Domain\Property\Property;
 
-final class RentalProperty
+final class RentalProperty extends Property
 {
-    private $id;
-    private $title;
-    private $description;
-    private $characteristics;
-    private $location;
-    private $published_at;
 
-    public function __construct(
-        PropertyId $id,
-        PropertyTitle $title,
-        PropertyDescription $description,
-        PropertyCharacteristics $characteristics,
-        PropertyLocation $location,
-        PropertyGallery $gallery,
-        DateTime $published_at = new DateTime()
-    ) {
-        $this->id = $id;
-        $this->title = $title;
-        $this->description = $description;
-        $this->characteristics = $characteristics;
-        $this->location = $location;
-        $this->gallery = $gallery;
-        $this->published_at = $published_at;
-    }
-
-    public function id(): PropertyId
-    {
-        return $this->id;
-    }
-    public function title(): PropertyTitle
-    {
-        return $this->title;
-    }
-    public function description(): PropertyDescription
-    {
-        return $this->description;
-    }
-    public function characteristics(): PropertyCharacteristics
-    {
-        return $this->characteristics;
-    }
-    public function location(): PropertyLocation
-    {
-        return $this->location;
-    }
-    public function gallery(): PropertyGallery
-    {
-        return $this->gallery;
-    }
-    public function published_at(): DateTime
-    {
-        return $this->gallery;
-    }
 }
---
+++ src/Catalog/Shared/Domain/Property/Property.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Catalog\Shared\Domain\Property;
+
+use DateTime;
+
+abstract class Property
+{
+    private $id;
+    private $title;
+    private $description;
+    private $characteristics;
+    private $location;
+    private $published_at;
+
+    public function __construct(
+        PropertyId $id,
+        PropertyTitle $title,
+        PropertyDescription $description,
+        PropertyCharacteristics $characteristics,
+        PropertyLocation $location,
+        PropertyGallery $gallery,
+        PropertyPrice $price_month,
+        DateTime $published_at = new DateTime()
+    ) {
+        $this->id = $id;
+        $this->title = $title;
+        $this->description = $description;
+        $this->characteristics = $characteristics;
+        $this->location = $location;
+        $this->gallery = $gallery;
+        $this->price_month = $price_month;
+        $this->published_at = $published_at;
+    }
+
+    public function id(): PropertyId
+    {
+        return $this->id;
+    }
+    public function title(): PropertyTitle
+    {
+        return $this->title;
+    }
+    public function description(): PropertyDescription
+    {
+        return $this->description;
+    }
+    public function characteristics(): PropertyCharacteristics
+    {
+        return $this->characteristics;
+    }
+    public function location(): PropertyLocation
+    {
+        return $this->location;
+    }
+    public function gallery(): PropertyGallery
+    {
+        return $this->gallery;
+    }
+    public function priceMonth(): PropertyPrice
+    {
+        return $this->price_month;
+    }
+    public function publishedAt(): DateTime
+    {
+        return $this->published_at;
+    }
+}
