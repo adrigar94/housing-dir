49dd70dc8cc18d83f9feaba54bda63f4c814145e
Adrián García <akrian231@gmail.com>
2022-11-26 16:58:54 +0100

    listener KernelException parse to JsonResponse

--- src/Catalog/RentalProperty/Domain/RentalPropertyNotExist.php
+++ src/Catalog/RentalProperty/Domain/RentalPropertyNotExist.php
@@ -3,18 +3,14 @@
 namespace App\Catalog\RentalProperty\Domain;
 
 use App\Catalog\Shared\Domain\Property\PropertyId;
-use App\Tests\Shared\Domain\DomainError;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
-final class RentalPropertyNotExist extends DomainError
+final class RentalPropertyNotExist extends HttpException
 {
     public function __construct(private readonly PropertyId $id)
     {
-        parent::__construct();
-    }
-
-    public function errorCode(): string
-    {
-        return 'rental_property_not_exist';
+        parent::__construct(Response::HTTP_NOT_FOUND, $this->errorMessage());
     }
 
     protected function errorMessage(): string
---
+++ src/Shared/Domain/Event/KernelException.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Shared\Domain\Event;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
+
+class KernelException
+{
+    public function onKernelException(ExceptionEvent $event): void
+    {
+        $request   = $event->getRequest();
+        // if ('application/json' !== $request->headers->get('Content-Type')) {
+        //     return;
+        // }
+
+        $exception = $event->getThrowable();
+
+        $data = [
+            'class' => \get_class($exception),
+            'code' => JsonResponse::HTTP_INTERNAL_SERVER_ERROR,
+            'message' => $exception->getMessage(),
+            'traces' => $exception->getTrace()
+        ];
+        
+        if ($exception instanceof HttpExceptionInterface) {
+            $data = [
+                'code' => $exception->getStatusCode(),
+                'message' => $exception->getMessage()
+            ];
+        }
+        
+        $event->setResponse($this->prepareResponse($data));
+    }
+
+    private function prepareResponse(array $data): JsonResponse
+    {
+        return new JsonResponse($data, $data['code']);
+    }
+}
