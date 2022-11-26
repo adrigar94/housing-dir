af987aeb6bd47c9b0ae81d4b49be40310aa7ca73
Adrián García <akrian231@gmail.com>
2022-11-26 16:00:34 +0100

    refactor migration

--- src/Shared/Infrastructure/Persistence/Doctrine/Migrations/Version20221126145601.php
+++ src/Shared/Infrastructure/Persistence/Doctrine/Migrations/Version20221126145601.php
@@ -14,7 +14,7 @@ final class Version20221126145601 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return '';
+        return 'Create new table rental_properties';
     }
 
     public function up(Schema $schema): void
@@ -26,7 +26,6 @@ final class Version20221126145601 extends AbstractMigration
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE SCHEMA public');
         $this->addSql('DROP TABLE rental_properties');
     }
 }
