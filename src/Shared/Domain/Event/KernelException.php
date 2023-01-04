afa764f0409eb76ab5929e6c0042729dc8538d9e
Adrián García <akrian231@gmail.com>
2023-01-04 19:44:24 +0100

    refactor: simplify return exception

--- src/Shared/Domain/Event/KernelException.php
+++ src/Shared/Domain/Event/KernelException.php
@@ -13,26 +13,18 @@ class KernelException
     public function onKernelException(ExceptionEvent $event): void
     {
         $request   = $event->getRequest();
-        // if ('application/json' !== $request->headers->get('Content-Type')) {
-        //     return;
-        // }
+        if ('application/json' !== $request->headers->get('Content-Type')) {
+            return;
+        }
 
         $exception = $event->getThrowable();
 
         $data = [
             'class' => \get_class($exception),
-            'code' => JsonResponse::HTTP_INTERNAL_SERVER_ERROR,
+            'code' => $exception->getCode() ?: JsonResponse::HTTP_INTERNAL_SERVER_ERROR,
             'message' => $exception->getMessage(),
             'traces' => $exception->getTrace()
         ];
-        
-        if ($exception instanceof Exception
-        ) {
-            $data = [
-                'code' => $exception->getCode(),
-                'message' => $exception->getMessage()
-            ];
-        }
         
         $event->setResponse($this->prepareResponse($data));
     }
